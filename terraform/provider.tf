473a7263ce6e177d2c8af6642fad0bcad4615333
Ravid Shachar <ravidshachar47@gmail.com>
2022-01-28 11:12:41 +0000

    Exchange installation sort of working! requires flag

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "=2.61.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.61.0"
     }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
